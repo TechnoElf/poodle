47890171e393734e800a7e386432794da4796728
TechnoElf <technoelf@undertheprinter.com>
2020-11-26 09:34:37 +0100

    Add timer and group permutation functionality

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,8 @@ async fn main() {
 
 struct Handler {
     subscribers: Arc<Mutex<HashMap<ChannelId, Vec<MoodleCourseData>>>>,
-    conf: Arc<Conf>
+    conf: Arc<Conf>,
+    groups: Arc<Mutex<Vec<String>>>
 }
 
 #[async_trait]
@@ -79,6 +80,7 @@ impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         let subscribers = self.subscribers.clone();
         let conf = self.conf.clone();
+        let groups = self.groups.clone();
 
         let words = msg.content.split(" ").collect::<Vec<_>>();
         if (msg.content.starts_with(&format!("<@!{}>", conf.discord_client_id)) ||
@@ -101,7 +103,7 @@ impl EventHandler for Handler {
                                         subscribers.insert(msg.channel_id, vec![course]);
                                     }
 
-                                    if let Err(e) = msg.channel_id.say(&ctx.http, get_resp(&conf)).await {
+                                    if let Err(e) = msg.channel_id.say(&ctx.http, format!("{} (watching course {})", get_resp(&conf), id)).await {
                                         eprintln!("Error sending message: {}", e);
                                     }
                                     println!("Channel {} is watching course {}", msg.channel_id, id);
@@ -113,22 +115,77 @@ impl EventHandler for Handler {
                         }
                     }
                 }
-            }
-
-            if cmd == "unwatch" && words.len() >= 3 {
+            } else if cmd == "unwatch" && words.len() >= 3 {
                 for word in words[2..].iter() {
                     if let Ok(id) = word.parse::<u32>() {
                         if let Some(cache) = self.subscribers.lock().await.get_mut(&msg.channel_id) {
                             if let Some(course_index) = cache.iter().position(|e| e.id() == id) {
                                 cache.remove(course_index);
 
-                                if let Err(e) = msg.channel_id.say(&ctx.http, get_resp(&conf)).await {
+                                if let Err(e) = msg.channel_id.say(&ctx.http, format!("{} (no longer watching course {})", get_resp(&conf), id)).await {
                                     eprintln!("Error sending message: {}", e);
                                 }
                                 println!("Channel {} is no longer watching course {}", msg.channel_id, id);
                             }
                         }
                     }
+                }
+            } else if cmd == "timer" && words.len() == 3 {
+                if let Ok(time) = words[2].parse() {
+                    if let Err(e) = msg.channel_id.say(&ctx.http, format!("{} (timer set for {} seconds)", get_resp(&conf), time)).await {
+                        eprintln!("Error sending message: {}", e);
+                    }
+
+                    tokio::spawn(async move {
+                        sleep(Duration::from_secs(time));
+
+                        if let Err(e) = msg.channel_id.say(&ctx.http, format!("{} (timer done)", get_resp(&conf))).await {
+                            eprintln!("Error sending message: {}", e);
+                        }
+                    });
+                }
+            } else if cmd == "makegroups" && words.len() == 2 {
+                let mut groups = groups.lock().await.clone();
+                let group_size = (groups.len() as f32 * 2.0).log2().floor() as usize;
+                let group_count = groups.len() as usize / group_size;
+
+                // Random permutation by Fisher-Yates Shuffle
+                let mut perm: Vec<Vec<String>> = Vec::with_capacity(group_count);
+                for _ in 0..group_count {
+                    perm.push(Vec::with_capacity(group_size));
+                }
+
+                for i in 0..groups.len() {
+                    let j = thread_rng().gen_range(0, groups.len());
+                    perm[i % group_count].push(groups.remove(j));
+                }
+
+                if let Err(e) = msg.channel_id.say(&ctx.http, get_resp(&conf)).await {
+                    eprintln!("Error sending message: {}", e);
+                }
+
+                let mut text = String::new();
+                for (i, group) in perm.iter().enumerate() {
+                    let mut substring = String::new();
+                    for e in group {
+                        substring.push_str(&format!("{} ", e));
+                    }
+                    text.push_str(&format!("Group {}: {}\n", i + 1, substring));
+                }
+
+                if let Err(e) = msg.channel_id.say(&ctx.http, text).await {
+                    eprintln!("Error sending message: {}", e);
+                }
+            } else if cmd == "makegroups" && words.len() >= 3 {
+                let mut groups = groups.lock().await;
+
+                groups.clear();
+                for word in words[2..].iter() {
+                    groups.push(word.to_string());
+                }
+
+                if let Err(e) = msg.channel_id.say(&ctx.http, format!("{} (created group with {} members)", get_resp(&conf), groups.len())).await {
+                    eprintln!("Error sending message: {}", e);
                 }
             }
         }
@@ -139,7 +196,8 @@ impl Handler {
     fn new(conf: Conf) -> Self {
         Self {
             subscribers: Arc::new(Mutex::new(HashMap::new())),
-            conf: Arc::new(conf)
+            conf: Arc::new(conf),
+            groups: Arc::new(Mutex::new(Vec::new()))
         }
     } 
 }
